b12c2ebf34600339d40849ee6c1b62297ff462b5
BonnieNyambura <nyamburabonnie@gmail.com>
2020-09-11 11:55:26 +0300

    bootstrap node registration with momgodb

--- start.js
+++ start.js
@@ -1,1 +1,7 @@
 // export our app variable so that it can be imported and used in other files.
+
+const app = require('./app');
+
+const server = app.listen(3000, () => {
+    console.log(`Express is running on port ${server.address().port}`);
+});
