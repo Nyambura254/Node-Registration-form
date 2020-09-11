1f1c8470d68ed293537e2826cb59d46dcc9589a4
BonnieNyambura <nyamburabonnie@gmail.com>
2020-09-11 12:39:13 +0300

    post method for rser

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 // Here, we’re importing Express into our routes file and then grabbing the router from it. We then use the router to respond to any requests to the root URL (in this case http://localhost:3000) with an “It works!” message.
 const express = require('express');
+//Validating Form Input
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -9,10 +11,33 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('form', { title: 'Registration form' });
 });
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.render('form', { title: 'Registration form' });
-});
+// router.post('/', (req, res) => {
+//     console.log(req.body);
+//     res.render('form', { title: 'Registration form' });
+// });
+router.post('/',
+    [
+        check('name')
+            .isLength({ min: 1 })
+            .withMessage('Please enter a name'),
+        check('email')
+            .isLength({ min: 1 })
+            .withMessage('Please enter an email'),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            res.send('Thank you for your registration!');
+        } else {
+            res.render('form', {
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+            });
+        }
+    }
+);
 
 
 
