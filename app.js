372a7e1c272ee79f6a1eece8f3d5260e8699f63d
BonnieNyambura <nyamburabonnie@gmail.com>
2020-09-11 11:54:18 +0300

    bootstrap node registration with momgodb

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// importing both the express module and (the export value of) our routes file into the application. 
 const express = require('express');
 const routes = require('./routes/index');
 
