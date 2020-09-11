6552048fb03971720e4dbfc6e4bc43edc5f8f937
BonnieNyambura <nyamburabonnie@gmail.com>
2020-09-11 12:12:02 +0300

    templating with pug engine

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // importing both the express module and (the export value of) our routes file into the application. 
+
+
+
+// const express = require('express');
+// const routes = require('./routes/index');
+
+// const app = express();
+// app.use('/', routes);
+
+// module.exports = app;
+
+
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
+
+// configure app.js to use Pug as a layout engine and to look for templates inside the views folder
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use('/', routes);
 
 module.exports = app;
