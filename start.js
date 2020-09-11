df942988e823a8d084077d152b005d499b423618
BonnieNyambura <nyamburabonnie@gmail.com>
2020-09-11 13:43:05 +0300

    moongose schema to connect to mongodb

--- start.js
+++ start.js
@@ -1,7 +1,20 @@
-// export our app variable so that it can be imported and used in other files.
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection
+    .on('open', () => {
+        console.log('Mongoose connection open');
+    })
+    .on('error', (err) => {
+        console.log(`Connection error: ${err.message}`);
+    });
+require('./models/Registration');
 const app = require('./app');
-
 const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
 });
